134c65258689e4480d910a8d85830c6fe940b5fe
achouksey <achouksey@achoukseys-MacBook-Pro.local>
2026-01-28 22:20:01 -0600

    Fix: Enable opening side panel on icon click, add icons

--- src/background/service-worker.js
+++ src/background/service-worker.js
@@ -1,1 +1,6 @@
+// Enable side panel click behavior
+chrome.sidePanel
+    .setPanelBehavior({ openPanelOnActionClick: true })
+    .catch((error) => console.error(error));
+
 console.log('Background service worker loaded.');
